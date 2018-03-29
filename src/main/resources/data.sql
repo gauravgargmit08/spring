16c101a9e1dba3b1f1266096657a70de97f9d3d8
gauravgargmit08 <37900048+gauravgargmit08@users.noreply.github.com>
2018-03-29 18:20:17 +0530

    Update data.sql
    Adding scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,18 +15,241 @@ VALUES(4,  'Haryana', 'smoker',200000,1,10,6000,'ICICI');
 
 
 INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
-VALUES(5,  'Delhi', 'smoker',200000,2,10,5000,'HDFC');
+VALUES(5,  'Delhi', 'smoker',200000,2,10,10000,'HDFC');
 
 
 INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
-VALUES(7,  'Delhi', 'smoker',200000,2,10,6000,'ICICI');
+VALUES(6,  'Delhi', 'smoker',200000,2,10,12000,'ICICI');
 
 INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
-VALUES(8,  'Haryana', 'smoker',200000,2,10,5500,'HDFC');
+VALUES(7,  'Haryana', 'smoker',200000,2,10,11000,'HDFC');
 
 
 INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
-VALUES(9,  'Haryana', 'smoker',200000,2,10,6000,'ICICI');
+VALUES(8,  'Haryana', 'smoker',200000,2,10,13000,'ICICI');
+
+
+
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(9,  'Delhi', 'smoker',200000,1,15,15000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(10,  'Delhi', 'smoker',200000,1,15,18000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(11,  'Haryana', 'smoker',200000,1,15,13500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(12,  'Haryana', 'smoker',200000,1,15,13000,'ICICI');
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(13,  'Delhi', 'smoker',200000,2,15,15000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(14,  'Delhi', 'smoker',200000,2,15,12000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(15,  'Haryana', 'smoker',200000,2,15,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(16,  'Haryana', 'smoker',200000,2,15,17000,'ICICI');
+
+
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(17,  'Delhi', 'smoker',200000,1,20,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(18,  'Delhi', 'smoker',200000,1,20,20000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(19,  'Haryana', 'smoker',200000,1,20,19500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(20,  'Haryana', 'smoker',200000,1,20,22000,'ICICI');
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(21,  'Delhi', 'smoker',200000,2,20,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(22,  'Delhi', 'smoker',200000,2,20,20000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(23,  'Haryana', 'smoker',200000,2,20,19500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(24,  'Haryana', 'smoker',200000,2,20,22000,'ICICI');
+
+
+
+---Non Smoker
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(25,  'Delhi', 'nonsmoker',200000,1,10,5000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(26,  'Delhi', 'nonsmoker',200000,1,10,6000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(27,  'Haryana', 'nonsmoker',200000,1,10,5500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(28,  'Haryana', 'nonsmoker',200000,1,10,6000,'ICICI');
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(29,  'Delhi', 'nonsmoker',200000,2,10,10000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(30,  'Delhi', 'nonsmoker',200000,2,10,12000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(31,  'Haryana', 'nonsmoker',200000,2,10,11000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(32,  'Haryana', 'nonsmoker',200000,2,10,13000,'ICICI');
+
+
+
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(33,  'Delhi', 'nonsmoker',200000,1,15,15000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(34,  'Delhi', 'nonsmoker',200000,1,15,18000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(35,  'Haryana', 'nonsmoker',200000,1,15,13500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(36,  'Haryana', 'nonsmoker',200000,1,15,13000,'ICICI');
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(37,  'Delhi', 'nonsmoker',200000,2,15,15000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(38,  'Delhi', 'nonsmoker',200000,2,15,12000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(39,  'Haryana', 'nonsmoker',200000,2,15,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(40,  'Haryana', 'nonsmoker',200000,2,15,17000,'ICICI');
+
+
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(41,  'Delhi', 'nonsmoker',200000,1,20,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(42,  'Delhi', 'nonsmoker',200000,1,20,20000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(43,  'Haryana', 'nonsmoker',200000,1,20,19500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(44,  'Haryana', 'nonsmoker',200000,1,20,22000,'ICICI');
+
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(45,  'Delhi', 'nonsmoker',200000,2,20,18000,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(46,  'Delhi', 'nonsmoker',200000,2,20,20000,'ICICI');
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(47,  'Haryana', 'nonsmoker',200000,2,20,19500,'HDFC');
+
+
+INSERT INTO quotes_info(ID, location, tobacco, amount,insurance_type,tenure,basepremium,companyname) 
+VALUES(48,  'Haryana', 'nonsmoker',200000,2,20,22000,'ICICI');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
